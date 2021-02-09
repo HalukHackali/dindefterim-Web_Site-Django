5ed7967d0415b58f1bad6aaf0129a643a5681dd2
Haluk <=>
2021-02-10 02:30:26 +0300

    iletişim modeli ve admin düzenlemesi yapıldı

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import KategoriModel, YazilarModel, YorumModel
+from blog.models import KategoriModel, YazilarModel, YorumModel, IletisimModel
 
 # Register your models here.
 
@@ -15,5 +15,11 @@ class YorumAdmin(admin.ModelAdmin):
     list_display = ('yazan', 'olusturulma_tarihi', 'duzenlenme_tarihi')
     search_fields = ('yazan__username',)
 
-
 admin.site.register(YorumModel, YorumAdmin)
+
+
+class IletisimAdmin(admin.ModelAdmin):
+    list_display = ('email', 'olusturulma_tarihi')
+    search_fields = ('email',)
+
+admin.site.register(IletisimModel, IletisimAdmin)
