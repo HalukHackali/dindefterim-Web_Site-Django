f9b8155093ed4e5771533f21b2b749e39540b2ad
Haluk <=>
2021-02-10 02:13:31 +0300

    yorum modeli admin ayarları yapıldı

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from blog.models import KategoriModel
-from blog.models import YazilarModel
+from blog.models import KategoriModel, YazilarModel, YorumModel
 
 # Register your models here.
 
@@ -11,3 +10,10 @@ class YazilarAdmin(admin.ModelAdmin):
     list_display = ('baslik', 'olusturulma_tarihi', 'duzenlenme_tarihi', 'yazar')
 
 admin.site.register(YazilarModel, YazilarAdmin)
+
+class YorumAdmin(admin.ModelAdmin):
+    list_display = ('yazan', 'olusturulma_tarihi', 'duzenlenme_tarihi')
+    search_fields = ('yazan__username',)
+
+
+admin.site.register(YorumModel, YorumAdmin)
