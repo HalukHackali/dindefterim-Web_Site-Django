56d29716f47d4990cf13ac82e9803c56e2b6770f
Haluk <=>
2021-02-11 01:45:18 +0300

    geçici anasayfa - arama işlevi - detay sayfası - iletişim sayfası - kategori sayfası - yazılar sayfas

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from blog.views import iletisim, anasayfa
+from django.urls import path, include
+from blog.views import kategori, yazilarim, anasayfa, iletisim, detay
+
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
-    path('iletisim', iletisim, name='iletisim'),
+    path('iletisim', iletisim, name= 'iletisim'),
+    path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
+    path('yazilarim', yazilarim, name='yazilarim'),
+    path('detay/<slug:slug>', detay, name='detay'),
 ]
--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from blog.models.yazi import YazilarModel
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def anasayfa(request):
-    return render(request, 'pages/anasayfa.html', context={})
+    sorgu = request.GET.get('sorgu')
+    yazilar = YazilarModel.objects.order_by('-id')
+
+    if sorgu:
+        yazilar = yazilar.filter(
+            Q(baslik__icontains=sorgu) |
+            Q(icerik__icontains=sorgu)
+        ).distinct()
+
+    sayfa = request.GET.get('sayfa')
+    paginator = Paginator(yazilar, 2)
+
+    return render(request, 'pages/anasayfa.html', context={
+        'yazilar' : paginator.get_page(sayfa)
+    })
