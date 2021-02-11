ce87678f6553fb8396f3bc2932de751a18288d14
Haluk <=>
2021-02-11 16:07:22 +0300

    iletişim form düzenlendi

--- blog/forms/iletisim.py
+++ blog/forms/iletisim.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class IletisimForm(forms.Form):
-    email = forms.EmailField(max_length=50)
-    isim_soyisim = forms.CharField(max_length=25)
-    mesaj = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField(label='E-posta', max_length=50)
+    isim_soyisim = forms.CharField(label='İsim Soyisim',max_length=25)
+    mesaj = forms.CharField(label='Mesajınız',widget=forms.Textarea(attrs={
+        'class': 'form-control'
+    }))
