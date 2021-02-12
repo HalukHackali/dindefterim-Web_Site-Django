02a93286de8872c429fece0bfb53cb860183ae6c
Haluk <=>
2021-02-12 21:12:34 +0300

    Hakkımda Sayfası Yapıldı

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from blog.views import kategori, yazilarim, anasayfa, iletisim, detay, yorum_sil, yazi_sil, yazi_guncelle, yazi_ekle
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
+    path('hakkimda', TemplateView.as_view(template_name="pages/hakkimda.html"), name='hakkimda'),
     path('iletisim', iletisim, name= 'iletisim'),
     path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
     path('yazilarim', yazilarim, name='yazilarim'),
