052727d7a831e750c458deec5a4c2957abde8272
Haluk <=>
2021-02-14 23:10:31 +0300

    Environ Dosyası Eklendi - SECRET_KEY yapılandırılması yapıldı.

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z1%qs_!%8ig9-03(t0ov+les!tw%z7=errxmm56-^7pq^homkz'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +48,7 @@ INSTALLED_APPS = [
     # third party apps
     'ckeditor',
     'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
