b92dbb392f25492e9ac38d76559bd6e6865d70e5
Haluk <=>
2021-02-14 17:53:44 +0300

    Yazı Güncelle Sayfasına YaziEkleCreateView(LoginRequiredMixin, CreateView): Eklendi

--- blog/views/yazi_guncelle.py
+++ blog/views/yazi_guncelle.py
@@ -3,10 +3,11 @@ from django.contrib.auth.decorators import login_required
 from blog.models import YazilarModel
 from blog.forms import YaziGuncelleFormModel
 from django.views.generic import UpdateView
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from  django.contrib.auth.mixins import LoginRequiredMixin
 
-
-class YaziGuncelleUpdateView(UpdateView):
+class YaziGuncelleUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('giris')
     template_name = 'pages/yazi-guncelle.html'
     fields = ('baslik', 'icerik', 'resim', 'kategoriler')
 
@@ -22,6 +23,25 @@ class YaziGuncelleUpdateView(UpdateView):
         return reverse('detay', kwargs={
             'slug': self.get_object().slug
         })
+
+
+
+# class YaziGuncelleUpdateView(UpdateView):
+#     template_name = 'pages/yazi-guncelle.html'
+#     fields = ('baslik', 'icerik', 'resim', 'kategoriler')
+#
+#     def get_object(self):
+#         yazi = get_object_or_404(
+#             YazilarModel,
+#             slug=self.kwargs.get('slug'),
+#             yazar=self.request.user
+#         )
+#         return yazi
+#
+#     def get_success_url(self):
+#         return reverse('detay', kwargs={
+#             'slug': self.get_object().slug
+#         })
 
 
 ############## CLASS BASE VIEW OLMADAN KULLANIM#################
