66e5e3cfdd6bd2bf5c84b90a2f42c03e4cfda943
Haluk <=>
2021-02-14 17:55:16 +0300

    Yazı Sil Sayfasına YaziEkleCreateView(LoginRequiredMixin, CreateView): Eklendi

--- blog/views/yazi_sil.py
+++ blog/views/yazi_sil.py
@@ -3,8 +3,10 @@ from blog.models import YazilarModel
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import DeleteView
 from django.urls import reverse_lazy
+from  django.contrib.auth.mixins import LoginRequiredMixin
 
-class YaziSilDeleteView(DeleteView):
+class YaziSilDeleteView(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('giris')
     template_name = 'pages/yazi-sil-onay.html'
     success_url = reverse_lazy('yazilarim')
 
@@ -12,6 +14,14 @@ class YaziSilDeleteView(DeleteView):
         yazi = YazilarModel.objects.filter(slug=self.kwargs['slug'], yazar=self.request.user)
         return yazi
 
+
+# class YaziSilDeleteView(DeleteView):
+#     template_name = 'pages/yazi-sil-onay.html'
+#     success_url = reverse_lazy('yazilarim')
+#
+#     def get_queryset(self):
+#         yazi = YazilarModel.objects.filter(slug=self.kwargs['slug'], yazar=self.request.user)
+#         return yazi
 
 ############## CLASS BASE VIEW OLMADAN KULLANIM#################
 # @login_required(login_url='/')
