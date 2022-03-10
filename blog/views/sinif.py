b77ac01c5fe2581d6e6ca351089f9e5a997a6791
Haluk H <halukh@protonmail.com>
2022-03-10 20:05:55 +0300

    ufak düzeltmeler yapıldı

--- blog/views/sinif.py
+++ blog/views/sinif.py
@@ -1,20 +1,17 @@
-from django.shortcuts import render, get_object_or_404
-from blog.models import YazilarModel, SinifModel
-from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
+
+from blog.models import SinifModel
+
 
 class SinifListView(ListView):
     template_name = 'pages/sinif.html'
     context_object_name = 'yazilar'
     paginate_by = 5
 
-
-
     def get_queryset(self):
         sinif = get_object_or_404(SinifModel, slug=self.kwargs['sinifSlug'])
         return sinif.yazi.filter(yayinlandi=True).order_by('id')
-
-
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(SinifListView, self).get_context_data(**kwargs)
